4306290dc388c582a24f627f2e7fd6b988fbc3c8
xiaoxineryi <529086017@qq.com>
2020-06-18 22:52:02 +0800

    修改了添加物品无法显示的BUG

--- src/main/java/com/xiaoxin/ac/Config/ResourceConfig.java
+++ src/main/java/com/xiaoxin/ac/Config/ResourceConfig.java
@@ -10,7 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/imgs/");
+        String root = "E:\\Grade_2\\WEB\\ac\\src\\main\\resources\\imgs\\";
+
+        registry.addResourceHandler("/img/**").addResourceLocations("file:"+root);
 
     }
 }
